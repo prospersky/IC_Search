eb0720b9770ab640cd1b7bca243ca77b6535a78d
Hochang Ryu <37101210+prospersky@users.noreply.github.com>
2022-05-30 11:22:21 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <MainHeader> </MainHeader>
+    <img src="./assets/logo.png">
+    <img src="./assets/logo.jpg">
+    <PictureInput></PictureInput>
+  </div>
+</template>
+
+<script>
+import MainHeader from './components/MainHeader.vue'
+import PictureInput from './components/PictureInput.vue'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+    'PictureInput' : PictureInput,
+    'MainHeader' : MainHeader
+  }
+}
+</script>
+
+<style>
+body {
+  text-align: center;
+  background-color: #f6f6f8
+}
+input {
+  border-style: grove;
+  width: 200px;
+}
+button {
+  border-style: groove;
+}
+.shawdow{
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03)
+}
+
+img {
+  width: 300px;
+  height: 150px;
+  object-fit: contain;
+}
+
+</style>
---
+++ src/components/PictureInput.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    picture select
+    <button v-on:click="uploadPicture">Upload</button>
+    <!-- <input id="input" type="file" accept="text/*" /> -->
+    <input id="input" type="file" @change="onFileSelectd"/>
+    <button @click="onUpload">Upload2</button>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      selectedFile: null
+    }
+  },
+  methods: {
+    uploadPicture() {
+      console.log("click");
+      var inputEl = document.querySelector("#input")
+      inputEl.addEventListener("change", function (event) {
+        // FileList
+        var fileList = inputEl.files || event.target.files
+        // File
+        var file = fileList[0]
+        var reader = new FileReader()
+        reader.onload = function (progressEvent) {
+          console.log(progressEvent.target.result)
+        }
+        reader.readAsText(file)
+      })
+    },
+    onFileSelectd(event) {
+      // console.log(event)
+      this.selectedFile = event.target.files[0]
+    },
+    onUpload() {
+
+    }
+  }
+}
+</script>
+
+<style scope>
+  input {
+    background-color: blue;
+    vertical-align: middle;
+  }
+</style>
